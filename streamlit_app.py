369dbb4780771d776bb5899fcc813cec7bbe79ac
jackycoulet-max <jackycoulet@gmail.com>
2026-07-10 15:50:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,15 @@ mois = st.sidebar.selectbox("Mois :", ["Janvier", "Février", "Mars", "Avril", "
 legume = st.sidebar.text_input("Entrez un légume , un fruit , une fleur, un arbuste , un arbre ou une plante aromatique) :", "Poireau")
 
 if st.sidebar.button("Ecoute la terre qui te parle"):
-    try:
+   try:
         client = genai.Client(api_key=API_KEY)
         response = client.models.generate_content(
             model='gemini-2.5-flash',
-            contents=f"Mois : {mois} | Légume : {legume}",
+            contents=f"Mois : {mois} | Produit : {legume}",
             config={'system_instruction': """Tu es un vieux jardinier sage et passionné du Jura. Structure ta réponse ainsi : 1. Focus principal : Parle ENTIÈREMENT de la plante, fleur, arbuste, arbre ou fruit demandé, avec des conseils (arrosage, soins, récolte) adaptés au climat jurassien pour le mois choisi. Ajoute une idée sympa (recette, astuce) si adapté. 2. Tour du jardin : Ajoute ensuite un court paragraphe 'Ailleurs dans le jardin ce mois-ci...' avec 2 ou 3 conseils généraux rapides."""}
         )
+        st.success("Les conseils du vieux sage :")
+        st.write(response.text)
     except Exception as e:
-        st.error(f"Erreur : {e}")
+        st.error(f"Oups, un petit souci technique : {e}")
             
